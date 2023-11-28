88b3ee383c0400e317e4ebf5d02d35a19b29f37f
shofiq <shofiqebr@gmail.com>
2023-11-28 12:35:51 +0600

    load data to client side

--- index.js
+++ index.js
@@ -24,13 +24,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const serviceCollection =client.db('touristGuideDB').collection('service');
+
+    app.get('/service',async(req,res)=>{
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result); 
+    })
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
