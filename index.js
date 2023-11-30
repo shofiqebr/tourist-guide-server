cdeb513e7dcb33770b619993d51a96f6bd071237
shofiq <shofiqebr@gmail.com>
2023-11-30 07:34:56 +0600

    get specific data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,14 @@ async function run() {
       res.send(result); 
     })
 
+
+       app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+         const result = await serviceCollection.findOne(query);
+            res.send(result);
+        })
 
 
 
